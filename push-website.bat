83239a4a87f9471cec6cf9a2ea2aa644364653f2
Elijah partney <elijahpartney9@gmail.com>
2023-04-21 19:24:08 -0500

    commit made by Elijah through .BAT

--- push-website.bat
+++ push-website.bat
@@ -3,11 +3,11 @@
 title Pushing to Github
 echo Getting Ready...
 F:\
-cd Coding\"calculator"
+cd Coding\"website"
 git add *
 git commit -m "commit made by Elijah through .BAT"
 git branch -M main
-git remote add origin https://github.com/ElijahPartney/Calculator.git
+git remote add origin https://github.com/ElijahPartney/Python-Website.git
 cls
 git push -u origin main
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Push Completed', 'Finished!', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
